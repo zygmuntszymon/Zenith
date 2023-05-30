bd3a3ee09742ff9f90ad7cce23cab14311cddd1a
Szymon Zygmunt <szymonzygmuntzsem@gmail.com>
2023-05-30 19:40:01 +0200

    sidebar, single post pt1

--- functions.php
+++ functions.php
@@ -21,6 +21,9 @@ function zenith_config()
     add_image_size('zenith-hero', 1920, 920, array(
         'center', 'center'
     ));
+    add_image_size('post-image', 900, 480, array(
+        'center', 'center'
+    ));
 }
 add_action('after_setup_theme', 'zenith_config', 0);
 
@@ -99,4 +102,22 @@ class bootstrap_5_wp_nav_menu_walker extends Walker_Nav_menu
 
         $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
     }
+}
+
+add_action('widgets_init', 'my_register_sidebars');
+function my_register_sidebars()
+{
+    /* Register the 'primary' sidebar. */
+    register_sidebar(
+        array(
+            'id'            => 'primary',
+            'name'          => __('Primary Sidebar'),
+            'description'   => __('A short description of the sidebar.'),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="sidebar-primary">',
+            'after_title'   => '</h3>',
+        )
+    );
+    /* Repeat register_sidebar() code for additional sidebars. */
 }
