4aa3053c14c929f236475b444c8f3ae39893afca
Szymon Zygmunt <szymonzygmuntzsem@gmail.com>
2023-06-02 22:21:29 +0200

    footer pt.1

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
-<footer>
-
+</main>
+</div>
+<footer id="footer" role="contentinfo">
+    <div id="copyright">
+        &copy; <?php echo esc_html(date_i18n(__('Y'))); ?>
+        <?php echo esc_html(get_bloginfo('name')); ?>
+    </div>
 </footer>
 </div>
 <?php wp_footer(); ?>
