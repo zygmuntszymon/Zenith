a300e2542fa09d5f4833495037522dd13c154e59
Szymon Zygmunt <szymonzygmuntzsem@gmail.com>
2023-07-17 14:06:46 +0200

    our-team page hero section and responsiveness

--- page-our-team.php
+++ page-our-team.php
@@ -7,9 +7,9 @@
         <button class="our-team-hero_cta"><i class="fa-regular fa-envelope"></i>&nbsp; CONTACT US TODAY</button>
     </div>
 </div>
-<div class="our-team container">
+<div class="our-team">
     <div class="member">
-        <img class="member-photo" src="<?php echo get_template_directory_uri() . '/img/p2.jpg' ?>" alt="Person 1">
+        <img class="member-photo" src="<?php echo get_template_directory_uri() . '/img/p2.png' ?>" alt="Person 1">
         <h2>Christina</h2>
         <p class="member-description">
             WooCommerce magician, crafting exceptional online stores. Mastering e-commerce, she weaves enchanting digital experiences for businesses.
@@ -20,7 +20,7 @@
         </div>
     </div>
     <div class="member">
-        <img class="member-photo" src="<?php echo get_template_directory_uri() . '/img/p1.jpg' ?>" alt="Person 1">
+        <img class="member-photo" src="<?php echo get_template_directory_uri() . '/img/p1.png' ?>" alt="Person 1">
         <h2>Christina</h2>
         <p class="member-description">
             WooCommerce magician, crafting exceptional online stores. Mastering e-commerce, she weaves enchanting digital experiences for businesses.
@@ -31,7 +31,7 @@
         </div>
     </div>
     <div class="member">
-        <img class="member-photo" src="<?php echo get_template_directory_uri() . '/img/p3.jpg' ?>" alt="Person 1">
+        <img class="member-photo" src="<?php echo get_template_directory_uri() . '/img/p3.png' ?>" alt="Person 1">
         <h2>Christina</h2>
         <p class="member-description">
             WooCommerce magician, crafting exceptional online stores. Mastering e-commerce, she weaves enchanting digital experiences for businesses.
